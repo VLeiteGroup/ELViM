c9cc15635bb49888ea10a6c16c95955a66727b05
VLeiteGroup <153555745+VLeiteGroup@users.noreply.github.com>
2023-12-11 15:26:15 -0600

    Add files via upload

---
+++ ELViM.py
@@ -0,0 +1,278 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 14 07:50:54 2023
+
+@author: rafael
+"""
+
+import argparse
+import numpy as np
+from numba import njit, prange
+import os
+import shutil
+import mdtraj as md
+import force_scheme as force
+
+@njit(parallel=True, fastmath=False)         ####dissimilarity defautl, generalizar para diferentes sigma_0 e epsilon
+def dissimilarity(coords,  s0):
+   """
+    Calculates the dissimilarity matrix using coordinates of C-alpha carbons.
+    Using other representations, such as all atoms or different number of 
+    coarse-grain beads, requires adjusting the residue number.
+    
+    Parameters
+    ----------
+    coords : np.float32
+        C-alpha atomic coordinates in Angstrom [frames, atom_index, (x, y, z)]
+    s0 : float
+        Gaussian width. Can be changed to gauge the dissimilarity. Default value is one.
+   """
+   epsilon=0.15
+   sigma_0=s0
+   size, atoms, _=coords.shape
+   total = int(size*(size+1)/2)
+   d=np.zeros(total, dtype=np.float32) 
+ 
+   for k in prange (size):
+      for l in prange (k, size):
+         N=0
+         ep=0
+         for i in prange(atoms):
+            for j in prange (i+1,atoms):
+               xijk = (coords[k][i][0] - coords[k][j][0])
+               yijk = (coords[k][i][1] - coords[k][j][1])
+               zijk = (coords[k][i][2] - coords[k][j][2])
+               rijk = np.sqrt(xijk*xijk + yijk*yijk + zijk*zijk)
+               xijl = (coords[l][i][0] - coords[l][j][0])
+               yijl = (coords[l][i][1] - coords[l][j][1])
+               zijl = (coords[l][i][2] - coords[l][j][2])
+               rijl = np.sqrt(xijl*xijl + yijl*yijl + zijl*zijl)
+               dr = (rijk-rijl)**2
+               sigma = sigma_0*abs(i-j)**epsilon 
+               ep = ep + np.exp(-dr/(2*sigma**2))
+               N = N + 1
+         d[int(total - ((size - k) * (size - k + 1) / 2) + (l - k))] = 1-ep/N
+
+   return d 
+
+
+@njit(parallel=True, fastmath=False)         ####dissimilarity defautl, generalizar para diferentes sigma_0 e epsilon
+def square_to_condensed(sqdmat):
+   """
+   Convert the symmetrical square matrix to the required condensed form.
+   """
+   size, _ = sqdmat.shape
+   total = size*(size+1)//2
+   d=np.zeros(total, dtype=np.float32) 
+   for k in prange (size):
+      for l in prange (k, size):
+         d[int(total - ((size - k) * (size - k + 1) / 2) + (l - k))] = sqdmat[k,l]
+   return d 
+
+def calc_dmat(coords, s0, outdm):
+    # Calculate the dissimilarity matriz
+    dmat = dissimilarity(coords,  s0)
+    if outdm!=None:
+        if os.path.exists(outdm):
+            backup_file(outdm)
+        np.save(outdm, dmat)
+        print("Dissimilarity matrix save in condensed form to the file {}".format(outdm))
+    return dmat
+    
+def get_coords(traj_file, top_file):
+    # Load the trajectory file
+        if traj_file[-4:] != '.pdb' and top_file==None:
+            raise RuntimeError("A topology file is needed!")
+        try:
+            if traj_file[-4:] == '.pdb':
+                traj = md.load(traj_file)
+            else:
+                traj=md.load(traj_file, top=top_file)
+            print("Trajectory succesfully loaded!")
+        except:
+            raise RuntimeError("Error loading trajectory.")
+        
+        # Select C-alpha and get coordinates in Angstroms
+        ca_indices=traj.topology.select("name == CA")
+        coords=traj.xyz
+        coords=coords[:,ca_indices,:]*10   # Angstrom
+        return coords
+
+def backup_file(file_path):
+      backup_path = file_path + '.bak'
+      shutil.move(file_path, backup_path)
+      print(f"Existing file '{file_path}' backed up to '{backup_path}'.")
+      
+def read_dmat(dmat_file):
+    try:
+        try:
+            dmat = np.load(dmat_file)
+        except:
+            dmat = np.loadtxt(dmat_file)
+    except:
+        raise RuntimeError("Failed to load the dissimilarity matrix from both binary .npy and text file formats.")
+
+    if len(dmat.shape)==1:
+        size = int(np.sqrt(2 * len(dmat)))
+        print("Loaded a condensed matrix for {} conformations".format(size))
+    elif len(dmat.shape)==2:
+        size, size2 =dmat.shape
+        if size!=size2:
+            raise RuntimeError("The dissimilarity matrix should be square (NxN)")
+        # Check if the loaded matrix is approximately symmetric
+        is_sym = np.allclose(dmat, dmat.T, atol=1e-3)
+        if not is_sym:
+            raise RuntimeError("The dissimilarity matrix should be symmetric")
+        dmat = square_to_condensed(dmat)
+        print("Loaded a square matrix for {} conformations".format(size))
+    else:
+        raise RuntimeError('''The dissimilarity matrix should be a symmetrical nxn or its condensed form.
+                              See the tutorial for details.''')
+    return dmat, size
+  
+
+def main():
+    """ Read either a trajectory or a dissimilarity matrix and output the projection.
+           
+    Args:
+        -f (filename): Pre-processed MD trajectory to be projected in any format recognized by MDtraj
+        -s (filename): Topology information (eg. frame.pdb) for loading the trajectory
+        -dm (filename): File containing a dissimilarity matrix (default: None)
+        -odm (filename): Name to save the dissimilarity matrix (default: dmat.npy)
+        -it (int): Maximum number of iteration (default: sqrt(n_frames))
+        -lr0 (int): Initial value for the learning_rate (default: 1/8)
+        -d (int): Exponential decay of the learning_rate (default: 0)
+        -s (int): random seed (default: None)
+        -op (filename): Name to save the ELViM projection coordinates file (default: projection.out)
+    Returns:
+        output (file): binary numpy file (.npy) containing the compressed dissimilarity matrix (default: coords.npy)
+        output (file): ELViM projection coordinates file (default: projection.out)
+    """
+    
+    message='''This program can:
+        1. Read the trajectory file (pdb or other formats +ref.pdb, calculate the dissimilarity matrix
+           and perform the ELViM projection. \n
+        2. Read a distance matrix in the symmetrical square or condensed format and perform the
+           ELViM projection.
+        * Either a trajectory or dissimilarity matrix should be provided.
+        * To save the dissimilarity matrix for other projections use -odm name.npy'''
+        
+    parser = argparse.ArgumentParser(description=message)
+    parser.add_argument("-f", dest="trajectory_file", 
+                        action="store", type=str, default=None,
+                        help="Name of the trajectory file")
+    parser.add_argument("-t", dest="topology_file", 
+                        action="store", type=str, default=None,
+                        help="Name of the topology file")
+    parser.add_argument("-dm", dest="dmat", 
+                  action="store", type=str, default=None,
+                  help="Name of the dissimilarity matrix file")
+    parser.add_argument("-c", dest="coords", 
+                  action="store", type=str, default=None,
+                  help="Name of the coordinate matrix file (MDtraj format")
+    parser.add_argument("-s0", dest="sigma0", 
+                        action="store", type=float, default=1.0,
+                        help="Value of sig0")
+    parser.add_argument("-it", dest="max_it", 
+                  action="store", type=int, default=None,
+                  help="Maximum number of iteration (default sqrt(n_frames")
+    parser.add_argument("-lr0", dest="learning_rate0", action="store", type=float, default=0.5,
+                  help="Initial learning rate")
+    parser.add_argument("-lrmin", dest="learning_rate_min", action="store", type=float, default=0.05,
+                  help="Minimum value for learning rate")
+    parser.add_argument("-d", dest="decay", action="store", type=float, default=0.95,
+                  help="Learnig rate exponential decay")
+    parser.add_argument("-tol", dest="tolerance", action="store", type=float, default=0,
+                  help="tolerance to achieve convergence")
+    parser.add_argument("-o", dest="output_proj",
+                  action="store", type=str, default="projection.out",
+                  help="ELViM projection coordinates")
+    parser.add_argument("-odm", dest="output_dmat",
+                        action="store", type=str, default=None,
+                        help="Condensed dissimilarity matrix in binary file")
+
+
+   
+    opt = parser.parse_args()
+    traj_file=opt.trajectory_file
+    top_file=opt.topology_file
+    dmat_file=opt.dmat
+    coords_file=opt.coords
+    s0 = opt.sigma0
+    max_it=opt.max_it
+    lr0=opt.learning_rate0
+    lrmin=opt.learning_rate_min
+    tol=opt.tolerance
+    decay=opt.decay
+    outdm=opt.output_dmat
+    outp=opt.output_proj
+
+    
+
+#### Check if only one of the possible files are parsed to avoid ambiguities between them.    
+    files = [traj_file, dmat_file, coords_file]
+    nfiles = sum(files is not None for files in files)
+    if nfiles == 0:
+        raise RuntimeError('''One of the following files should be provided:
+                              (1) A trajectory file (plus a topology)
+                              (2) A dissimilarity matrix 
+                              (3) A MDtraj coordinates file
+                              Try "python ELViM.py -h" to see the options. ''')
+    elif nfiles > 1:
+        raise RuntimeError('''Only one of the following files should be provided:
+                              (1) A trajectory file (plus a topology)
+                              (2) A dissimilarity matrix 
+                              (3) A MDtraj coordinates file).''')
+
+     #Read traj and calculate the dissimilarity matrix                         
+    if traj_file!=None:                       
+       coords=get_coords(traj_file, top_file)
+       dmat = calc_dmat(coords, s0, outdm)
+       size, atom, _=coords.shape
+       print('Dissimilaty matrix calculated considering {} conformations and {} alpha carbons'.format(size, atom))
+
+    elif dmat_file!=None:
+       dmat, size = read_dmat(dmat_file)
+       print('Dissimilaty matrix loaded. There are {} conformations'.format(size))
+       
+    elif coords_file!=None:
+       try:
+            coords = np.load(coords_file)
+            print(''''
+                  Coordinates matix loaded. 
+                  Make sure coordinates are for CA only and expressed in Angstroms.
+                  ''')        
+       except:
+            raise RuntimeError('''Failed to load the coords file. 
+                                  Make sure it is a binary numpy file (.npy),
+                                  and the matrix follows the MDtraj format trajectory.xyz*10''')
+       dmat = calc_dmat(coords, s0, outdm)
+       size, atom, _=coords.shape
+       print('Dissimilaty matrix calculated considering {} conformations and {} alpha carbons'.format(size, atom)) 
+  
+    ### Make the projection
+    ### Initializing the projection with points randomly distributed
+    projection = np.random.random((size, 2))   
+    if max_it==None:
+        max_it = int(np.sqrt(size))
+    
+    ### Running force scheme
+    nr_iterations, error, kstress = force.execute(dmat, projection, max_it, lr0, lrmin, decay, tol)
+   
+    ### write the projection coordinates file
+    if os.path.exists(outp):
+       backup_file(outp)
+    with open(outp, 'w') as f:
+       f.write("# ELViM Coordinates file. \n") 
+       if dmat_file==None:
+           f.write("# Dissimilarity matrix calculated with sigma0={} \n".format(s0))
+       else:
+           f.write("# Dissimilarity matrix read from the file : {}\n".format(dmat_file))
+       f.write("# {} iterations with lr0 = {:5.3f}, lrmin = {:5.3f}, and decay = {:5.3f} \n".format(nr_iterations, lr0, lrmin, decay))
+       f.write("# Mean projection error = {:6.4f}, Stress = {:6.4f} \n".format(error[nr_iterations-1], kstress[nr_iterations-1]))
+       np.savetxt(f, projection, delimiter=" ", fmt="%.4f")
+    
+if __name__=="__main__":
+	main()
+	exit(0)
---
+++ force_scheme.py
@@ -0,0 +1,89 @@
+import numpy as np
+import math
+from numba import njit, prange
+import mdtraj as md
+
+
+
+@njit(parallel=True)
+def stress(distance_matrix, projection):
+    size = len(projection)
+    total = len(distance_matrix)
+    den = 0
+    num = 0
+    for i in prange(size):
+        for j in prange(size):
+            dr2 = math.sqrt((projection[i][0] - projection[j][0]) * (projection[i][0] - projection[j][0]) +
+                            (projection[i][1] - projection[j][1]) * (projection[i][1] - projection[j][1]))
+
+            r = (i + j - math.fabs(i - j)) / 2  # min(i,j)
+            s = (i + j + math.fabs(i - j)) / 2  # max(i,j)
+            drn = distance_matrix[int(total - ((size - r) * (size - r + 1) / 2) + (s - r))]
+            num += (drn - dr2) * (drn - dr2)
+            den += drn * drn
+    return math.sqrt(num / den)
+
+
+@njit(parallel=True, fastmath=False)
+def move(ins1, distance_matrix, projection, learning_rate):
+    size = len(projection)
+    total = len(distance_matrix)
+    error = 0
+    
+    for ins2 in prange(size):
+        if ins1 != ins2:
+            x1x2 = projection[ins2][0] - projection[ins1][0]
+            y1y2 = projection[ins2][1] - projection[ins1][1]
+            dr2 = max(math.sqrt(x1x2 * x1x2 + y1y2 * y1y2), 0.0001)
+
+            # getting te index in the distance matrix and getting the value
+            r = (ins1 + ins2 - math.fabs(ins1 - ins2)) / 2  # min(ins1,ins2)
+            s = (ins1 + ins2 + math.fabs(ins1 - ins2)) / 2  # max(ins1,ins2)
+            drn = distance_matrix[int(total - ((size - r) * (size - r + 1) / 2) + (s - r))]
+
+            # calculate the movement
+            delta = (drn - dr2) #* math.fabs(drn - dr2)
+            
+            error += math.fabs(delta)
+
+            # moving
+            projection[ins2][0] += learning_rate * delta * (x1x2 / dr2)
+            projection[ins2][1] += learning_rate * delta * (y1y2 / dr2)
+    return error / (size-1)
+
+@njit(parallel=False, fastmath=False)
+def iteration(index, distance_matrix, projection, learning_rate):
+    size = len(projection)
+    error = 0
+
+    for i in range(size):
+        ins1 = index[i]
+        error += move(ins1, distance_matrix, projection, learning_rate)
+    return error / size 
+
+@njit(fastmath=False)
+def execute(distance_matrix, projection, max_it, learning_rate0=0.5, lrmin= 0.05, decay=0.95, tolerance=0.0000001):
+    nr_iterations = 0
+    size = len(projection)
+    error= math.inf
+    kstress=np.zeros(max_it)
+    p_error = np.zeros(max_it)
+    # create random index
+    index = np.random.permutation(size)
+    for k in range(max_it):
+        learning_rate = max(learning_rate0 * math.pow((1 - k / max_it), decay), lrmin)
+        new_error = iteration(index, distance_matrix, projection, learning_rate)
+        if math.fabs(new_error - error) < tolerance:
+            break
+        error = new_error
+        p_error[k] = error
+        kstress[k] = stress(distance_matrix, projection)
+        nr_iterations = k + 1  
+    # setting the min to (0,0)
+    min_x = min(projection[:, 0])
+    min_y = min(projection[:, 1])
+    for i in range(size):
+        projection[i][0] -= min_x
+        projection[i][1] -= min_y
+    return nr_iterations, p_error, kstress
+
